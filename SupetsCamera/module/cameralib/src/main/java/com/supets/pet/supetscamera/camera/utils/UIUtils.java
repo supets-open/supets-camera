8e917275872a2c65cfa120ec7ab2c19f23e9c575
李洪江 <lihongjiang@supets.com>
2017-07-29 15:03:07 +0800

    update

--- SupetsCamera/module/cameralib/src/main/java/com/supets/pet/supetscamera/camera/utils/UIUtils.java
+++ SupetsCamera/module/cameralib/src/main/java/com/supets/pet/supetscamera/camera/utils/UIUtils.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
 
-import com.supets.pet.supetscontext.App;
+import com.supets.lib.supetscontext.App;
 
 public class UIUtils {
 
